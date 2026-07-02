651864c83a34fbc0cd88479ad23b97c40a9e3abd
TusharChhapekar <tusharchhapekar642@gmail.com>
2026-07-02 11:54:29 +0530

    Refactor layout management and add label using place
    Removed commented-out layout management code and added a label using the place geometry manager.

--- tkinterPython.py
+++ tkinterPython.py
@@ -134,17 +134,6 @@ print(NoteBook.tabs())
 NoteBook.pack(expand=True, fill="both", side="top")
 
 
-#29-06-2026
-#Layout Management
-
-#pack()
-#Labelpack = tk.Label(win, text="textOfPack", bg="blue")
-#abelpack.pack()
-
-                                                    #sticky= specify specific position in grid cell
-tk.Label(win, text="Name").grid(row=2, column=2, sticky="nsew")     #layout using rows and columns
-#tk.Entry(win).grid(row=1, column=2)                 #recomment to use grid and pack in sepparate frame
-                                #row and colm bot strat from 0
 labelplace = tk.Label(win, text="Place position")
 labelplace.place(relx=0.7, y=99.9)                   #used to locate widget at exact position
                                                                 #relx rely (0.0 left or top, 0.5 center, 1.0 right or bottom)
